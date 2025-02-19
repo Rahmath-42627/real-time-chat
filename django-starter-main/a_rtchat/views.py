e318acf26da9228cf00734f12369500f7ca332f9
Rahmath-42627 <rahmathulla42627@gmail.com>
2025-02-19 20:50:12 +0530

    UPDATED

--- django-starter-main/a_rtchat/views.py
+++ django-starter-main/a_rtchat/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib.auth.decorators import login_required
 from .models import*
+from .forms import ChatmessageCreateForm
 
 # Create your views here.
 @login_required
 def chat_view(request):
     chat_group = get_object_or_404(ChatGroup, group_name="public-chat")
     chat_message = chat_group.chat_message.all()[:30]
-    return render(request,'a_rtchat/chat.html', {'chat_message': chat_message})
+    form = ChatmessageCreateForm()
+
+    if request.method == 'POST':
+        form= ChatmessageCreateForm(request.POST)
+        if form.is_valid:
+            message =form.save(commit=False)
+            message.author=request.user
+            message.group = chat_group
+            message.save()
+            return redirect('home')
+
+    return render(request,'a_rtchat/chat.html', {'chat_message': chat_message, 'form':form})
